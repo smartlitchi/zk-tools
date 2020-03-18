7e30617eed27b4f1392947b92333a8a23d65fc70
zimhat <zimhat@pm.me>
2020-03-18 15:43:45 +0100

    script renamed to zk_tools

--- tests/test_zk_tools.py
+++ tests/test_zk_tools.py
@@ -1,29 +1,29 @@
-import zkmanipulation
+import zk_tools
 import os
 
-zk_archive = os.path.expanduser("~/Projects/zettelkasten/tests/sources/")
+zk_archive = os.path.expanduser("~/Projects/zk_tools/tests/sources/")
 z_filename = '198707052100.rst'
 
 def test_get_title():
-    z_title = zkmanipulation.get_title(zk_archive, z_filename)
+    z_title = zk_tools.get_title(zk_archive, z_filename)
     assert z_title == 'Hello world !'
 
 def test_gen_slug():
-    z_title = zkmanipulation.get_title(zk_archive, z_filename)
-    z_slug = zkmanipulation.gen_slug(z_filename, z_title)
+    z_title = zk_tools.get_title(zk_archive, z_filename)
+    z_slug = zk_tools.gen_slug(z_filename, z_title)
     assert z_slug == '198707052100-hello-world.rst'
 
 def test_git_cmd():
     cmd = 'git status'
-    git_process = zkmanipulation.git_cmd(cmd, zk_archive)
+    git_process = zk_tools.git_cmd(cmd, zk_archive)
     assert git_process.returncode == 0
 
 def test_get_files_to_slug():
-    z_files = zkmanipulation.get_files_to_slug(zk_archive)
+    z_files = zk_tools.get_files_to_slug(zk_archive)
     assert len(z_files) == 1
     assert z_files[-1] == z_filename
 
 def test_zk_slugify():
-    zkmanipulation.zk_slugify(zk_archive)
+    zk_tools.zk_slugify(zk_archive)
     z_list = os.listdir(zk_archive)
     assert '198707052100-hello-world.rst' in z_list
