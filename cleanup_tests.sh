08dc41ec6098efb9df61e0b729c1e491a6f7e84d
zimhat <zimhat@pm.me>
2020-03-19 15:08:55 +0100

    cleaning up after tests updated

--- cleanup_tests.sh
+++ cleanup_tests.sh
@@ -3,3 +3,4 @@ git restore --staged tests/sources/198707052100.rst
 git restore --staged tests/sources/198707052100-hello-world.rst
 git restore tests/sources/198707052100.rst
 rm tests/sources/198707052100-hello-world.rst
+git checkout -- tests/sources/
